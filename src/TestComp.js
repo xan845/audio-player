1c169732ef124960dd339eb32212eec5d0d231aa
Paul S <dedsecdarknet@gmail.com>
2022-04-10 22:42:38 +0530

    vehicle modeled

--- src/TestComp.js
+++ src/TestComp.js
@@ -1,13 +1,42 @@
-import { useContext } from "react";
-import SaveContext from "./SaveContext";
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import { useState } from 'react';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
 
-function TestComp() {
-    const save = useContext(SaveContext);
-    return (
-        <div>
-            {JSON.stringify(save, null, 2)}
-        </div>
-    );
-}
+function App() {
+	const [key, setKey] = useState('junkyard');
+	return (
+		<Container className='p-3' fluid>
+			<Row className='p-3'>
+				<div className='text-center'>
+					<button onClick={() => setKey('partseller')}>Sell Now</button>
+				</div>
+			</Row>
+			<Tabs
+				id='main-view'
+				activeKey={key}
+				onSelect={k => setKey(k)}
+				className='mb-3'
+			>
+				<Tab eventKey='junkyard' title='Junkyard'>
+					Junkyard
+				</Tab>
+				<Tab eventKey='parkinglot' title='Parkinglot'>
+					Parkinglot
+				</Tab>
+				<Tab eventKey='workshop' title='Workshop'>
+					Workshop
+				</Tab>
+				<Tab eventKey='stockroom' title='Stockroom'>
+					Stockroom
+				</Tab>
+				<Tab eventKey='partseller' title='Partseller' disabled>
+					Partseller
+				</Tab>
+			</Tabs>
+		</Container>
+	);
+};
 
-export default TestComp;
+export default App;
---
+++ src/Vehicle.js
@@ -0,0 +1,29 @@
+class Vehicle {
+	constructor(lvl) {
+
+	}
+}
+
+const vehicle = {
+	id: Date.now().toString(36),
+	name: 'String name',
+	image: 'url or some static impls',
+	buy: 239.65,
+	move: 8,
+	work: 34,
+	stock: 10,
+	state: 0,
+	count: 0,
+	parts: [
+		{ time: 30, price: 0 }, // Engine
+		{ time: 30, price: 0 }, // Engine
+		{ time: 30, price: 0 }, // Engine
+		{ time: 30, price: 0 }, // Engine
+		{ time: 30, price: 0 }, // Engine
+		{ time: 30, price: 0 }, // Engine
+		{ time: 30, price: 0 }, // Engine
+		{ time: 30, price: 0 }, // Engine
+		{ time: 30, price: 0 }, // Engine
+		{ time: 30, price: 0 }, // Engine
+	]
+}
