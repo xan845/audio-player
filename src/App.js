a3be83394bc75c313aa1054d7c35a562b3602b61
Paul S <dedsecdarknet@gmail.com>
2022-04-24 16:21:07 +0530

    pwa-ed untested

--- src/App.js
+++ src/App.js
@@ -1,84 +1,147 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { arrayMove, arrayRemove, List } from "react-movable";
 
+const audio = new Audio();
+// audio.currentTime
+
+
 export default function App() {
-	const trackCurrent = 0.00;
-	const [tracks, setTracks] = useState([
-		{ name: "The Beatles Elanor Rigby", duration: 2.06 },
-		{ name: "The Rigby Beatles Elanor", duration: 1.27 },
-		{ name: "The Elanor Beatles Rigby", duration: 3.42 },
-		{ name: "The Beatles Rigby Elanor", duration: 4.17 },
-	]);
-	const [currentTrack, setCurrentTrack] = useState(null);
+
+	const files = useRef();
+
+	const [shuffle, setShuffle] = useState(false);
+	const [tracks, setTracks] = useState([]);
+	const [playing, nowPlaying] = useState({});
+
+	const playNext = () => {
+		const index = shuffle ? Math.random() * tracks.length | 0 : playing.index + 1;
+		const track = tracks[index];
+		console.log(tracks, index);
+		if (!track) setCurrentTrack(null);
+		else setCurrentTrack(track, index);
+	};
+
+	const playPrev = () => {
+		const index = shuffle ? Math.random() * tracks.length | 0 : playing.index - 1;
+		const track = tracks[index];
+		if (!track) setCurrentTrack(null);
+		else setCurrentTrack(track, index);
+	};
+
+	const setCurrentTrack = (file, index = 0) => {
+		audio.src = file ? URL.createObjectURL(file) : null
+		nowPlaying({ file, index, duration: 0, current: 0 });
+	};
+
+	const setDurations = () => nowPlaying(nP => {
+		URL.revokeObjectURL(audio.src);
+		audio.play();
+		return {
+			...nP,
+			duration: audio.duration,
+			current: audio.currentTime
+		};
+	});
+
+	const updateCurrent = () => nowPlaying(nP => ({ ...nP, current: audio.currentTime }));
+
+	const trackEnded = () => !audio.loop && playNext();
+
+	useEffect(() => {
+		audio.addEventListener('canplaythrough', setDurations)
+		audio.addEventListener('timeupdate', updateCurrent);
+		audio.addEventListener('ended', trackEnded)
+
+		return () => {
+			audio.removeEventListener('canplaythrough', setDurations);
+			audio.removeEventListener('timeupdate', updateCurrent);
+			audio.removeEventListener('ended', trackEnded);
+		};
+	}, []);
+
 	return (
-		<main className="player">
+		<>
+			<main className="player">
 
-			<header className="header">
-				<p>{currentTrack?.name ?? "Select a track"}</p>
-			</header>
-
-			<List
-				values={tracks}
-				onChange={({ oldIndex, newIndex }) => setTracks(arrayMove(tracks, oldIndex, newIndex))}
-				renderList={({ children, props }) => (<ul {...props} className="songs">{children}</ul>)}
-				renderItem={({ value, props, index }) => (
-					<li {...props} className="song">
-						<button data-movable-handle className="button button-sm">
-							<i className="fas fa-search" aria-hidden></i>
-							<span className="sr-only">M</span>
-						</button>
-						<p onClick={() => setCurrentTrack(value)}>{value.name}</p>
-						{
-							Object.is(currentTrack, value) ? (
-								<button className="button button-sm" onClick={() => setCurrentTrack(null)}>
+				<header className="header">
+					<p>{playing.file?.name ?? "Select a track"}</p>
+					<button className="button button-sm" onClick={() => files.current.click()}>
+						<i className="fas fa-search" aria-hidden></i>
+						<span className="sr-only">A</span>
+					</button>
+				</header>
+				<>
+					<List
+						values={tracks}
+						onChange={({ oldIndex, newIndex }) => {
+							const shifted = arrayMove(tracks, oldIndex, newIndex);
+							const index = shifted.findIndex(trx => Object.is(trx, playing.file));
+							nowPlaying(nP => ({ ...nP, index }));
+							setTracks(shifted);
+						}}
+						renderList={({ children, props }) => (<ul {...props} className="songs">{children}</ul>)}
+						renderItem={({ value, props, index }) => (
+							<li {...props} className="song">
+								<button data-movable-handle className="button button-sm">
 									<i className="fas fa-search" aria-hidden></i>
-									<span className="sr-only">S</span>
+									<span className="sr-only">M</span>
 								</button>
-							) : (
-								<button className="button button-sm" onClick={() => setTracks(arrayRemove(tracks, index))}>
-									<i className="fas fa-search" aria-hidden></i>
-									<span className="sr-only">D</span>
-								</button>
-							)
-						}
-					</li>
-				)}
-			/>
+								<p onClick={() => setCurrentTrack(value, index)}>{value.name}</p>
+								{
+									Object.is(playing.file, value) ? (
+										<button className="button button-sm" onClick={() => setCurrentTrack(null)}>
+											<i className="fas fa-search" aria-hidden></i>
+											<span className="sr-only">S</span>
+										</button>
+									) : (
+										<button className="button button-sm" onClick={() => setTracks(arrayRemove(tracks, index))}>
+											<i className="fas fa-search" aria-hidden></i>
+											<span className="sr-only">D</span>
+										</button>
+									)
+								}
+							</li>
+						)}
+					/>
+				</>
+				<aside className="prog">
+					<div className="prog-time">
+						{/* convert current to good format */}
+						<span className="left">{parseInt(playing.current/60, 10)}:{parseInt(playing.current%60)}</span>
+						<span className="right">{parseInt(playing.duration/60, 10)}:{parseInt(playing.duration%60)}</span>
+						{/* convert duration to good format */}
+					</div>
+					<div className="prog-bar">
+						<div className="prog-bar-inner" style={{ width: `${100 * (playing.current / playing.duration)}%`}}></div>
+					</div>
+				</aside>
 
-			<aside className="prog">
-				<div className="prog-time">
-					<p className="left">{currentTrack ? trackCurrent.toFixed(2) : '-.-'}</p>
-					<p className="right">{currentTrack?.duration.toFixed(2) ?? '-.-'}</p>
-				</div>
-				<div className="prog-bar">
-					<div className="prog-bar-inner"></div>
-				</div>
-			</aside>
+				<footer className="buttons">
+					<button className="button button-sm" onClick={() => setShuffle(s => !s)}>
+						<i className="fas fa-random fa-sm" aria-hidden></i>
+						<span className="sr-only">S</span>
+					</button>
+					<button className="button button-md" onClick={() => playPrev()}>
+						<i className="fas fa-step-backward" aria-hidden></i>
+						<span className="sr-only">P</span>
+					</button>
+					<button className="button button-lg" onClick={() => audio.paused ? audio.play() : audio.pause()}>
+						<i className="fas fa-pause fa-lg" aria-hidden></i>
+						<span className="sr-only">P</span>
+					</button>
+					<button className="button button-md" onClick={() => playNext()}>
+						<i className="fas fa-step-forward"></i>
+						<span className="sr-only">N</span>
+					</button>
+					<button className="button button-sm" onClick={() => audio.loop = !audio.loop}>
+						<i className="fas fa-circle-notch fa-sm" aria-hidden></i>
+						<span className="sr-only">L</span>
+					</button>
+				</footer>
 
-			<footer className="buttons">
-				<button className="button button-sm">
-					<i className="fas fa-random fa-sm" aria-hidden></i>
-					<span className="sr-only">S</span>
-				</button>
-				<button className="button button-md">
-					<i className="fas fa-step-backward" aria-hidden></i>
-					<span className="sr-only">P</span>
-				</button>
-				<button className="button button-lg">
-					<i className="fas fa-pause fa-lg" aria-hidden></i>
-					<span className="sr-only">P</span>
-				</button>
-				<button className="button button-md">
-					<i className="fas fa-step-forward"></i>
-					<span className="sr-only">N</span>
-				</button>
-				<button className="button button-sm">
-					<i className="fas fa-circle-notch fa-sm" aria-hidden></i>
-					<span className="sr-only">L</span>
-				</button>
-			</footer>
-
-		</main>
+			</main>
+			<input ref={files} onChange={e => setTracks(trx => [...trx, ...e.target.files])} type="file" accept="audio/*" multiple />
+		</>
 	)
 };
 /*
@@ -106,4 +169,13 @@ export default function App() {
 		</button>
 	</li>
 </ul>
+*/
+/*
+const trackCurrent = 0.00;
+const [tracks, setTracks] = useState([
+	{ name: "The Beatles Elanor Rigby", duration: 2.06 },
+	{ name: "The Rigby Beatles Elanor", duration: 1.27 },
+	{ name: "The Elanor Beatles Rigby", duration: 3.42 },
+	{ name: "The Beatles Rigby Elanor", duration: 4.17 },
+]);
 */
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import App from "./App";
 import "./global.css";
 
@@ -10,3 +11,8 @@ createRoot(
 		<App />
 	</StrictMode>
 );
+
+serviceWorkerRegistration.register({
+	onUpdate: sw => console.log('SW Update', sw),
+	onSuccess: sw => console.log('SW Success', sw)
+});
